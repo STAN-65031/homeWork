145130ced8442b9d8b1f14b46c1db7437429a087
STAN-65031 <lox.ibany@gmail.com>
2022-08-17 13:38:55 +0300

    сумма цифр числа

---
+++ homework2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int SumDigitsNumber (int number)
+{
+string str = number.ToString();
+char one;
+ int[] mass = new int[str.Length];
+for (int i = 0; i < str.Length; i++)
+{
+    one = str[i];
+   int gap =int.Parse(one.ToString());
+    mass[i] = gap; 
+}
+int result = 0;
+for (int j = 0; j < str.Length; j++)
+{
+    result = result+mass[j];
+}
+return result;
+}
+
+int resultNumber = SumDigitsNumber(N);
+Console.WriteLine(resultNumber);
